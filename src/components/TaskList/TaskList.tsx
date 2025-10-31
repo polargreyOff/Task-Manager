50e778cd54a71612bb93a0310f2c360939146eea
polargreyOff <spotmaster2017@yandex.ru>
2025-10-31 20:04:39 +0300

    added search for groups

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { fetchGroups } from "../../store/actions/groupsActions";
 import { Preloader } from "../../UI/Preloader";
@@ -9,7 +9,22 @@ const TaskList = () => {
    const groups = useAppSelector(store => store.groups);
     const loading = useAppSelector(store => store.UIstate.loading.groups);
     const error = useAppSelector(store => store.UIstate.errors.groups);
+    const [search, setSearch] = useState<string>("");
     const dispatch = useAppDispatch();
+
+    const searchGroups = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    }
+
+    const filteredGroups = useMemo(()=> {
+        if (search.trim().length === 0) {
+            return groups
+        }
+        return groups.filter(group => (
+            group.title.toLowerCase().includes(search.toLowerCase())
+        ))
+    }, [search, groups])
+
     useEffect(() => {
         if (groups.length === 0) {
             dispatch(fetchGroups());
@@ -27,9 +42,20 @@ const TaskList = () => {
         )
     }
     return (
-            <ul className={styles.row}>
-            {groups.map(group => (<TaskItem key={group.id} {...group}/>))}
-            </ul>
+        <div>
+            <div className={styles.input}>
+                <input onChange={searchGroups} placeholder="Поиск группы" type="text" name="search"/>
+            </div>
+            {filteredGroups.length > 0 ? (
+                <ul className={styles.row}>
+                    {filteredGroups.map(group => (<TaskItem key={group.id} {...group}/>))}
+                </ul>
+                
+            ): (
+                <p>Группы не найдены</p>
+            )}
+        </div>
+            
         
     )
 }
